b44533a832ba398d7c08b44e73496bc3f7cc6366
Vikash Choudhary <imvikashhereyo@gmail.com>
2023-05-06 22:18:43 +0530

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import Nav from '@components/Nav';
 import '@styles/globals.css';
+import { AppProps } from 'next/app';
+import { PropsWithChildren } from 'react';
 
 export const metaData = {
   title: 'Promptopia',
   description: 'Promptopia is a tool to create and share prompts',
 };
 
-const RootLayout = ({ children }: { children: JSX.Element }) => {
+const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="en">
       <head></head>
