9569a7c51a55f3a0c9b7bf4a21384b5b09953e9e
Vikash Choudhary <imvikashhereyo@gmail.com>
2023-05-23 22:31:38 +0530

    create-prompt end point added

---
+++ app/api/prompt/new/route.ts
@@ -0,0 +1,19 @@
+import Prompt from '@models/prompt';
+import { connectToDatabase } from '@utils/database';
+
+export const POST = async (req: Request, res: Response) => {
+  const { userId, prompt, tag } = await req.json();
+  try {
+    await connectToDatabase();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (e) {
+    return new Response('Failed to create a new prompt', { status: 500 });
+  }
+};
